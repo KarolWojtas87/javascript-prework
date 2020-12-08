13b7c5d036f379ac65bcaa3e32423511b17a2b23
Karol Wojtaś <karol.wojtas87@gmail.com>
2020-12-08 18:51:28 +0100

    Add everything before styling

--- js/script.js
+++ js/script.js
@@ -14,16 +14,13 @@ function getMoveName(moveId)
     }
     else
     {
-        return 'błąd'
+        'błąd'
     }
 }
-let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-let computerMove = getMoveName(randomNumber);
-/*
 function displayResult(computerMove, playerMove)
 {
-    printMessage('Zagrałem ' + computerMove + 'a Ty ' + playerMove);
+    printMessage('Zagrałem ' + computerMove + ', a Ty ' + playerMove);
 
         if( computerMove == 'kamień' && playerMove == 'papier')
     {
@@ -65,40 +62,25 @@ function displayResult(computerMove, playerMove)
     {
         printMessage('Brak roztrzygnięcia');
     }
-}*/
-
-
-/*let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
-console.log('Gracz wpisał: ' + playerInput);
-
-let playerMove = 'nieznany ruch';
-
-if(playerInput == 1){
-  playerMove = 'kamień';
 }
-else if(playerInput == 2){
-  playerMove = 'papier';
-}
-else if(playerInput == 3){
-  playerMove = 'nożyce';
-}
-*/
 
-console.log('Wylosowana liczba to: ' + randomNumber);
+    document.getElementById('button-papier').addEventListener('click', function(){
+    playGame('papier');
+  });
+    document.getElementById('button-kamien').addEventListener('click', function(){
+    playGame('kamień');
+  });
+    document.getElementById('button-nozyce').addEventListener('click', function(){
+    playGame('nożyce');
+  });
 
+function playGame(playerMove)
+{
+    clearMessages();
 
-/*if(randomNumber == 1){
-  computerMove = 'kamień';
-}
-else if(randomNumber == 2){
-  computerMove = 'nożyce';
-}
-else if(randomNumber == 3){
-  computerMove = 'papier';
-}
-else {computerMove = 'błąd';
-     }*/
-printMessage('Ruch gracza: ' + playerMove);
-printMessage('Ruch komputera: ' + computerMove);
+    let randomNumber = Math.floor(Math.random() * 3 + 1);
 
+    let computerMove = getMoveName(randomNumber);
+
+    displayResult(computerMove, playerMove);
+}

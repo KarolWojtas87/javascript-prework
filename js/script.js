7a37a0bf30ea049bae7424e0891629834120dfbb
Karol Wojtaś <karol.wojtas87@gmail.com>
2020-12-08 12:54:52 +0100

    Add all moves

--- js/script.js
+++ js/script.js
@@ -1,4 +1,64 @@
-let playerMove = 'papier';
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyczki, to wygrywasz!');
+console.log('Gracz wpisał: ' + playerInput);
 
+let playerMove = 'nieznany ruch';
+
+if(playerInput == 1){
+  playerMove = 'kamień';
+}
+else if(playerInput == 2){
+  playerMove = 'papier';
+}
+else if(playerInput == 3){
+  playerMove = 'nożyce';
+}
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+else if(randomNumber == 2){
+  computerMove = 'nożyce';
+}
+else if(randomNumber == 3){
+  computerMove = 'papier';
+}
+else {computerMove = 'błąd';
+     }
+printMessage('Ruch gracza: ' + playerMove);
+printMessage('Ruch komputera: ' + computerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+  }
+  else if( computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Remis!');
+  }
+  else if( computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('Ty przegrywasz!');
+  }
+  else if( computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Remis!');
+  }
+  else if( computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('Ty przegrywasz!');
+  }
+  else if( computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+  }
+  else if( computerMove == 'nożyce' && playerMove == 'papier'){
+    printMessage('Ty przegrywasz!');
+  }
+  else if( computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+  }
+  else if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+    printMessage('Remis!');
+  }
+  else 
+  printMessage('Brak roztrzygnięcia');
